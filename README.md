0c46226078d8aedce7096ebd28be5f4c51d87308
ossboard <ossboard@gmail.com>
2022-01-10 19:36:16 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# docker-gitlab
+
+## 실행방법
+
+```
+mkdir GITLAB_HOME
+chmod 777 GITLAB_HOME
+sh startup.sh up (docker-compose up -d)
+(한10분후, http://127.0.0.1:8080 접속)
+
+* root 패스워드
+docker exec -it gitlab cat /etc/gitlab/initial_root_password 
+
+```
+
+## root로그인후
+```
+1.우상단 설정 (preferences)
+ - 테마는 Dark Mode, 문법 Dark 테마
+ - 언어는 Korea
+
+2.좌상단 menu > Admin
+ - 좌메뉴 설정 > 가입제한 > 신규가입시관리자승인(체크없앰,Require admin approval for new sign-ups) 
+
+```
+
+## 가입
+```
+First,Last Name:  sbs/pds
+Username: sbspds
+Email: sbspds@sbspds.com
+Password: sbspdssbspds
+
+(필요시, root 로그인후, 승인)
+1. 한글로 변경
+2. Create new Project > Crete from template > GitLab Cluster Management
+ - Project name: sbspds
+ - Visibility Level: Public
+
+
+
+
+
+```
---
+++ startup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+export userid=$(whoami)
+if [ ${userid} == 'root' ]
+then
+  echo "root 권한으로 실행할 수 없습니다"
+  exit 0
+fi
+export T_USAGE="Usage: $0 {start | stop | restart | status | log | shell | up(install)| down(uninstall)  }"
+export T_HOME=`pwd -P`
+
+cd ${T_HOME}
+
+fn_start() {
+  docker-compose start
+}
+
+fn_stop() {
+  docker-compose stop
+}
+
+fn_status() {
+  docker-compose ps
+}
+
+fn_log() {
+  echo "fn_log"
+}
+
+fn_shell() {
+ docker exec -it gitlab /bin/bash
+}
+
+
+fn_up() {
+  docker-compose up -d
+  fn_shell
+}
+
+fn_down() {
+  docker-compose down
+}
+
+case $1 in
+  start) fn_start ;;
+  stop) fn_stop ;;
+  restart)
+      fn_stop
+      sleep 3
+      fn_start ;;
+  status) fn_status ;;
+  log) fn_log ;;
+  shell) fn_shell ;;
+  up) fn_up ;;
+  down) fn_down ;;
+  *) echo ${T_USAGE} ;;
+esac
+exit 0
